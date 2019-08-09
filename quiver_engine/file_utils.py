eff6f6c0df107477762b9bea801262e5cbc33c67
faridi94 <48013336+faridi94@users.noreply.github.com>
2019-08-09 16:17:12 +0200

    Update file_utils.py

--- quiver_engine/file_utils.py
+++ quiver_engine/file_utils.py
@@ -8,15 +8,15 @@ from quiver_engine.util import deprocess_image
 def save_layer_img(layer_outputs, layer_name, idx, temp_folder, input_path):
     filename = get_output_filename(layer_name, idx, temp_folder, input_path)
     imsave(filename, deprocess_image(layer_outputs))
-    return relpath(filename, abspath(temp_folder))
+    return relpath(filename, abspath(temp_folder)).replace("#", "%23")
 
 def get_output_filename(layer_name, z_idx, temp_folder, input_path):
     return '{}/{}_{}_{}.png'.format(temp_folder, layer_name, str(z_idx), input_path)
 
 def list_img_files(input_folder):
-    image_regex = re.compile(r'.*\.(jpg|png|gif)$')
+    image_regex = re.compile(r'.*\.(jpg|png|gif|bmp)$')
     return [
-        filename
+        filename.replace("#", "%23")
         for filename in listdir(
             abspath(input_folder)
         )
