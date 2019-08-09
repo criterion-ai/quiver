03e3700098961f445f131768b9c1cdab8c54ae52
faridi94 <48013336+faridi94@users.noreply.github.com>
2019-08-09 16:21:58 +0200

    Update example.py

--- quiver_engine/example.py
+++ quiver_engine/example.py
@@ -1,12 +1,8 @@
-from scipy.misc import imsave
 from quiver_engine import server
-
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import CustomObjectScope
 from tensorflow.keras.initializers import glorot_uniform
-from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
 import numpy as np
 
 model = load_model('C:/Users/jenni/Downloads/artifacts_model.h5')
